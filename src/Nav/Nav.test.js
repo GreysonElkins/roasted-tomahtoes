872a3f81e18478983067cc167899f03660fefb7f
Leigh Larson <leighlarson4@gmail.com>
2020-08-18 19:44:08 -0600

    Revert "DRY up Nav tests"
    This reverts commit 8b6e29ac949df1d728bc804a7921d5e065ca9fb1.

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -4,35 +4,41 @@ import Nav from './Nav'
 import '@testing-library/jest-dom'
 
 describe('Nav', () => {
-  let nav, buttons, getByText
-  beforeEach(() => {
-    nav = render(<Nav />)
-    buttons = nav.getAllByRole('button')
-    getByText = text => nav.getByText(text)
-  })
+  // let buttons, nav
+  // beforeEach(() => {
+  //   nav = getAllByRole
+  //   // const getByText = render(<Nav/>)
+  //   buttons = getAllByRole('buttons')
+  // })
+
 
   it('should have four buttons', () => {
-    expect(buttons.length).toBe(4)
+    const {getAllByRole} = render(<Nav />)
+    expect(getAllByRole('button').length).toBe(4)
   })
 
   it('should have a Home button', () => {
+   const { getByText, getAllByRole } = render(<Nav />)
    expect(getByText('Home')).toBeInTheDocument()
-   expect(buttons[0].id).toBe('home-btn')
+   expect(getAllByRole('button')[0].id).toBe('home-btn')
   });
 
   it('should have a Your Ratings button', () => {
+   const { getByText, getAllByRole } = render(<Nav />)
    expect(getByText('Your Ratings')).toBeInTheDocument()
-   expect(buttons[1].id).toBe('ratings-header-btn')
+   expect(getAllByRole('button')[1].id).toBe('ratings-header-btn')
   });
 
   it('should have a Login button', () => {
+    const { getByText, getAllByRole } = render(<Nav />)
     expect(getByText('Login')).toBeInTheDocument()
-    expect(buttons[2].id).toBe('login-btn');
+    expect(getAllByRole('button')[2].id).toBe('login-btn');
   });
 
   it('should have a Logout button', () => {
+   const { getByText, getAllByRole } = render(<Nav />)
    expect(getByText('Logout')).toBeInTheDocument()
-   expect(buttons[3].id).toBe('logout-btn');
+   expect(getAllByRole('button')[3].id).toBe('logout-btn');
   });
 
 

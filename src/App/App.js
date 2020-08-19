924fc9af217d5f7451719f3ce443e3fcbe8acddf
Leigh Larson <leighlarson4@gmail.com>
2020-08-18 20:23:31 -0600

    Add class shell to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.scss'
 import Header from '../Header/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  App() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    )
+  }
 }
+
 
 export default App

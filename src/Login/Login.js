4871baedbc4c01656dcd7014eeb122348a73caf6
Leigh Larson <leighlarson4@gmail.com>
2020-08-19 16:59:18 -0600

    Init login class and folders

---
+++ src/Login/Login.js
@@ -0,0 +1,11 @@
+import React, {component, Component} from 'react'
+import './Login.scss'
+
+class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+}

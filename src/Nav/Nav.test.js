c973184cda6415755e8684f1134098250e75c6f8
Leigh Larson <leighlarson4@gmail.com>
2020-08-18 18:16:01 -0600

    Add tests to Nav

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,4 +1,30 @@
 import { render } from '@testing-library/react'
 import React from 'react'
-import Nav from './Nav/Nav'
+import Nav from './Nav'
 import '@testing-library/jest-dom'
+
+describe('Nav', () => {
+
+  it("should have a home button", () => {
+   const { getByText } = render(<Nav />);
+   expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  it("should have a Your Ratings button", () => {
+   const { getByText } = render(<Nav />);
+   expect(getByText("Your Ratings")).toBeInTheDocument();
+  });
+
+  it('should have a Login button', () => {
+    const { getByText } = render(<Nav />)
+    expect(getByText('Login')).toBeInTheDocument()
+  });
+
+  it("should have a logout button", () => {
+   const { getByText } = render(<Nav />);
+   expect(getByText("Logout")).toBeInTheDocument();
+  });
+
+
+
+})

f877a039c6f6c5a44fc7c363af81d831e0636e5e
Leigh Larson <leighlarson4@gmail.com>
2020-08-20 19:12:17 -0600

    Refactor Main component for readability

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,21 +4,19 @@ import './Main.scss'
 import api from '../API/API'
 import Error from '../Error/Error'
 
-const Main = (props) => {
-
+const Main = ({error, movies, showMoviePage}) => {
+  const movieCards = movies.map(movie => {
+      return <MovieCard 
+        movie={movie} 
+        showMoviePage={showMoviePage}
+    />
+  })
   return (
     <main>
-      {props.error && <Error error={props.error} />}
+      {error && <Error error={error} />}
       {/* <h2 className='all-movies-header'>All Movies</h2><br/> */}
       <section className='gallery'>
-        {
-          props.movies.map(movie => {
-            return <MovieCard 
-              movie={movie} 
-              showMoviePage={props.showMoviePage}
-          />
-        })
-      }
+        {movieCards}
       </section>
     </main>
   )

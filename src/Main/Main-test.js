817aaa435eb2fbfd3845266ffdbd026de686ff09
Leigh Larson <leighlarson4@gmail.com>
2020-08-21 18:03:21 -0600

    Init test for Main

--- src/Main/Main-test.js
+++ src/Main/Main-test.js
@@ -1,2 +1,39 @@
-//test for a movies array,
-// test that it can recieve props (mocking)
+import { render, getByPlaceholderText, screen } from "@testing-library/react";
+import React from "react";
+import Main from "./Main";
+import "@testing-library/jest-dom";
+
+describe("MoviePage", () => {
+ let movies;
+ beforeEach(() => {
+  const movies = [
+   {
+    id: 1,
+    title: "Donkey Kong",
+    poster_path: "someURL",
+    backdrop_path: "someURL",
+    release_date: "2019-12-04",
+    overview: "Some overview",
+    average_rating: 6,
+   },
+   {
+    id: 2,
+    title: "Wizard of Oz",
+    poster_path: "someURL",
+    backdrop_path: "someURL",
+    release_date: "1983-10-05",
+    overview: "Some overview",
+    average_rating: 4,
+   },
+  ];
+  render(
+   <Main
+    movies={movies}
+    showMoviePage={jest.fn()}
+    error={'No movies found'}
+   />
+  );
+ });
+
+ it("should render a number of movies equal to the length of the array being passed in", () => {});
+});

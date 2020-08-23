bc67aa09e7170fb6c8bbbe00c11e6a2bc4a120ea
Greyson Elkins <greysonelkins@gmail.com>
2020-08-22 18:17:19 -0600

    Test and update API, prep sad-paths

--- src/API/API-test.js
+++
@@ -1,4 +0,0 @@
-// mock out an api response
-// have to up on async await in testing suites (do we need JEST here?)
-// check the return
-// do we want to implement error handling here, instead of app? 
--- src/API/API.js
+++ src/API/API.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 const apiHead = 'https://rancid-tomatillos.herokuapp.com/api/v2'
 class API {
+  // constructor() {
+    // this.apiHead = 'https://rancid-tomatillos.herokuapp.com/api/v2'
+    // can I not use this in static methods because the API wasn't constructed?
+  // }
 
   static getData = async location => {
     let response
@@ -14,33 +18,70 @@ class API {
       return error
     }
     return data[key]
+    //should this get moved into the `try` block?
   }
   
   static findRelevantData = location => {
+    const acceptableData = ['movies', 'movie', 'videos', 'ratings']
     const apiPath = location.split('/') 
-    if (apiPath.length === 3) {
+    if (apiPath.length === 3 
+      && acceptableData.includes(apiPath[2])) {
       return apiPath[2]
-    } else if (apiPath.length === 2) {
+    } else if (apiPath.length === 2 
+      && acceptableData.includes('movies')) {
       return 'movie'
-    } else {
+    } else if (location.includes('movies')) {
       return 'movies'
+    } else {
+      throw new Error("A bad path was provided for fetching data")
     }
   }
 
   static postData = async (info, id) => {
-    const path = id ? `/users/${id}/ratings` : 'login'
+    if (this.postInfoIsOk(info, id)) {
+      const path = id ? `users/${id}/ratings` : 'login'
+      try {
+        const response = await fetch(`${apiHead}/${path}`, {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(
+            info
+          )
+        })
+        return response
+      } catch (error) {
+        return error
+      }
+    }
+  }
+
+  static postInfoIsOk = (info, id) => {
+    const acceptableUserInfo = ['email', 'password']
+    const acceptableRatingInfo = ['rating', 'movie_id']
+    const infoValues = Object.keys(info)
+    if (id && infoValues.every(
+      value=> acceptableRatingInfo.includes(value))) {
+      return true
+    } else if (infoValues.every(
+      value => acceptableUserInfo.includes(value))) {
+      return true
+    } else {
+      throw new Error ('Something is wrong with the data for POST')
+    }
+  }
+
+  static deleteData = async (userID, ratingID) => {
     try {
-      const response = await fetch(`${apiHead}/${path}`, {
-        method: 'POST',
+      const response = await fetch(`${apiHead}/users/${userID}/ratings/${ratingID}`, {
+        method: 'DELETE',
         headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(
-          info
-        )
-      })
+          'Content-Type': 'application/json',
+        }
+      })  
       return response
-    } catch (error) {
+    } catch(error) {
       return error
     }
   }

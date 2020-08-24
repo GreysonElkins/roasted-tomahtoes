4e44250ec6766ba259c2f3be928e8ed86c12932f
Leigh Larson <leighlarson4@gmail.com>
2020-08-24 03:21:43 -0600

    Add toggle functionality to toggle button text

--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -5,18 +5,19 @@ import PropTypes from 'prop-types'
 class Overview extends Component {
   constructor(props) {
     super(props)
-    this.state = {collapsed: true, }
+    this.state = {collapsed: true}
   }
 
   toggleOverview = () => {
-    this.setState({collapsed: !this.state.collapsed})
+    this.setState({
+      collapsed: !this.state.collapsed})
   }
 
   render() {
     return(
       <span className='overview-box'>
           <p className={this.state.collapsed ? 'movie-overview' : 'movie-overview-exp'}>{this.props.movie.overview}</p>
-          <button className='moreLessOverview' onClick={this.toggleOverview}>More Info</button>
+          <button className='moreLessOverview' onClick={this.toggleOverview}>{this.state.collapsed ? 'Show More' : 'Show Less'}</button>
       </span>
 
     )

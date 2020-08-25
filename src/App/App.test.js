3f0423921d389e67f8257a32238be53b22dac5c7
Leigh Larson <leighlarson4@gmail.com>
2020-08-24 18:13:32 -0600

    Pass some App tests, need to pass fetch test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,35 +1,84 @@
 import React from 'react'
+// import React-DOM from 'react-dom'
 import { render, screen, getByPlaceholderText, getByText, waitFor } from '@testing-library/react'
 import App from './App'
-// import Header from '../Header/Header'
 import '@testing-library/jest-dom'
+import {getData} from '../API/API'
+jest.mock('../API/API.js')
+
 
 describe('App', () => {
-  let movies
  beforeEach(() => {
-  movies = [ 
-
-  ]
-
+  //  mockGetData = jest.mock(...) 
    render(<App/>)
  })
 
-  it('should start with default login state', () => {
+  it('should start with default state', () => {       
     const defaultApp = new App()
     expect(defaultApp.state.isLoggedIn).toBe(false)
+    expect(defaultApp.state.movies).toStrictEqual([])
+    expect(defaultApp.state.error).toBe('')
+    expect(defaultApp.state.pageView).toBe('Home')
+    expect(defaultApp.state.userRatings).toStrictEqual([])
+    expect(defaultApp.state.singleMovie).toStrictEqual({})
+    expect(defaultApp.state.singleMovieUserRating).toStrictEqual({})
+    expect(defaultApp.state.trailers).toStrictEqual([])
+    expect(defaultApp.state.user).toStrictEqual({id: '', email: '', name: ''})
+    const {getByText} = render(<App/>)
+    expect(getByText("Your Ratings").className).toBe("hidden")
+    expect(getByText('Logout').className).toBe('hidden')
   })
 
   it('should render header', () => {
-    expect(screen.getByPlaceholderText('Search by title, genre, year')).toBeInTheDocument()
+    const {getByPlaceholderText} = render(<App/>)
+    expect(getByPlaceholderText('Search by title, genre, year')).toBeInTheDocument()
   })
 
-  it('should hide some header elements in default state', () => {
-    expect(screen.getByText("Your Ratings").className).toBe("hidden");
-    expect(screen.getByText('Logout').className).toBe('hidden')
+  it('should show some header elements if isLoggedIn is true', () => {
+    const { getByText } = render(<App isLoggedIn={true}/>)
+    expect(getByText("Your Ratings")).toBeInTheDocument()
+    expect(getByText('Logout')).toBeInTheDocument()
   })
 
-  it('should fetch movies data on load', async () => {
-    const movie1 = await waitFor(() => screen.getByText('Akira'))
+  it('should render fetched movies data on load', async () => {
+    getData.mockResolvedValueOnce([{
+      id: 1,
+      title: "Donkey Kong",
+      poster_path: "someURL",
+      backdrop_path: "someURL",
+      release_date: "2019-12-04",
+      overview: "Some overview",
+      average_rating: 6,
+    },
+      {
+        id: 2,
+        title: "Godzilla",
+        poster_path: "someURL",
+        backdrop_path: "someURL",
+        release_date: "2019-12-04",
+        overview: "Some overview",
+        average_rating: 4,
+      }, 
+      {
+        id: 3,
+        title: "Signs",
+        poster_path: "someURL",
+        backdrop_path: "someURL",
+        release_date: "2018-12-04",
+        overview: "Some overview",
+        average_rating: 8,
+      }
+  ]);
+
+    const {getByText} = render(<App/>)
+    const movie1 = await waitFor(() => getByText('Donkey Kong'))
+    const movie2 = await waitFor(() => getByText('Godzilla'))
+    const movie3 = await waitFor(() => getByText('Signs'))
+
+    expect(movie1).toBeInTheDocument()
+    expect(movie2).toBeInTheDocument()
+    expect(movie3).toBeInTheDocument()
+
   })
 
   //component did mount

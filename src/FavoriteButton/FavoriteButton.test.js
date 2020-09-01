3f284deed8715c30fb9fffcb1072908a81a47bd5
Greyson Elkins <greysonelkins@gmail.com>
2020-08-31 22:03:16 -0600

    Create FavoriteButton tests

--- src/FavoriteButton/FavoriteButton.test.js
+++ src/FavoriteButton/FavoriteButton.test.js
@@ -1,12 +1,55 @@
 import {
   render,
-  getByPlaceholderText,
   getByText,
-  getByRole,
   screen,
-  within,
+  fireEvent
 } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import React from "react";
-import MoviePage from "./MoviePage";
+import FavoriteButton from "./FavoriteButton";
 import "@testing-library/jest-dom";
+
+describe('Favorite buttons', () => {
+  
+  let mockToggleFavorite
+
+  beforeEach(() => {
+    mockToggleFavorite = jest.fn()
+  })
+
+  it('should render with an empty heart if not a favorite', () => {
+    render(
+      <FavoriteButton
+        toggleFavorite={mockToggleFavorite}
+        isFavorite={false}
+      />
+    )
+    const icon = screen.getByText("🤍");
+    expect(icon).toBeInTheDocument()
+  })
+
+  it("should render with an selected heart if a favorite", () => {
+    render(
+      <FavoriteButton
+        toggleFavorite={mockToggleFavorite}
+        isFavorite={true}
+      />
+    )
+    const icon = screen.getByText("❤️")
+    expect(icon).toBeInTheDocument()
+  })
+
+  it('should call toggleFavorite when clicked', () => {
+    render(
+      <FavoriteButton
+        toggleFavorite={mockToggleFavorite}
+        isFavorite={true}
+        movie={{id: 1}}
+      />
+    )
+    const icon = screen.getByText("❤️");
+    fireEvent.click(icon)
+    expect(mockToggleFavorite).toBeCalledTimes(1)
+  })
+
+})
